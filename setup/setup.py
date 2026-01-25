8aad8c8c96f2b600965d475de158f0ef92359111
Ivan Becerra <ivan3becerra@gmail.com>
2026-01-25 17:15:19 -0500

    Latest Stable Update

--- setup/setup.py
+++ setup/setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="tonalpohualli",
-    version="0.1",
+    version="0.1.0",
+    description="Tonalpohualli calendar engine with trecenas, nemontemi, and ritual cycles",
+    author="Ivan Becerra",
     packages=find_packages(),
+    python_requires=">=3.10",
 )
